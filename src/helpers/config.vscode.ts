2e6ceb8651a3cc6f041db8162829e4765a3674ec
lijian01 <lijian01@kanzhun.com>
2022-08-24 20:18:22 +0800

    调整配置名

--- src/helpers/config.vscode.ts
+++ src/helpers/config.vscode.ts
@@ -1,6 +1,6 @@
 import * as vscode from 'vscode';
 
-const THEME_FILE_PATH = 'bs.css.variable';
+const THEME_FILE_PATH = 'bs.css.variables';
 
 export const getThemePathConfig = (): string => {
   const config: string = vscode.workspace.getConfiguration().get(THEME_FILE_PATH) || '';
