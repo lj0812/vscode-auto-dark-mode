e7de25c5a9a029e9d668c7bb27f7765b14ddbd03
lijian01 <lijian01@kanzhun.com>
2022-08-24 21:04:04 +0800

    降级vscode engines

--- src/helpers/config.vscode.ts
+++ src/helpers/config.vscode.ts
@@ -6,7 +6,7 @@ export const getThemePathConfig = (): string => {
   const config: string = vscode.workspace.getConfiguration().get(THEME_FILE_PATH) || '';
 
   if (!config) {
-    vscode.window.showErrorMessage('请配置CSS变量文件路径：bs.css.variable', { modal: true });
+    vscode.window.showErrorMessage(`请配置CSS变量文件路径：${THEME_FILE_PATH}`, { modal: true });
   }
 
   return config;
